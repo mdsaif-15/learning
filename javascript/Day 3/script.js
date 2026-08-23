fa7a33e440ec333817e2d8e34519168d5b1d3d5f
mohdsaifansari191-art <mohdsaifansari191@gmail.com>
2026-08-23 10:40:17 +0530

    Q1 - Day 03

---
+++ javascript/Day 3/script.js
@@ -0,0 +1,23 @@
+// Q1. Write a regular function multiply(a, b) 
+// that returns the product of two numbers. 
+// Now rewrite it as an arrow function with 
+// an explicit return, and again using implicit return.
+
+// Answer :-
+
+// function multiply(a, b) {
+//     return a * b;
+// }
+// let a = prompt("enter the value for A");        //----- regular function.
+// let b = prompt("enter the value for B");        
+
+// console.log(multiply(a, b));
+
+// const multiply = (a, b) => {
+//     return a * b;                                   //---- Explicit return 
+// }                                                   //---- arrow function.
+
+// console.log(multiply(a, b));
+
+// const multiply = (a, b) => a * b;                      //---- implicit return.
+
