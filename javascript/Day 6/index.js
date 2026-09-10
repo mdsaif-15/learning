8ede59fb1f1cc526374a68d592b824df8ac6fa7c
mohdsaifansari191-art <mohdsaifansari191@gmail.com>
2026-09-11 01:22:02 +0530

    Day 6

--- javascript/Day 6/index.js
+++ javascript/Day 6/index.js
@@ -1,9 +1,24 @@
-const click = document.querySelector("#button");
+const innerDiv = document.querySelector(".inner-loader");
+const p = document.querySelector(".p");
+const btn = document.querySelector(".btn");
 
-click.addEventListener("click", function () {
-    console.log("clicked")
-    click.innerHTML = "Downloaded"
-    click.style.backgroundColor = "lightgreen"
+let a = 0;
+let c = (b = Math.floor(Math.random() * 10)) ? b : 0
+
+btn.addEventListener("click", function () {
+    btn.style.pointerEvents = "none"
+    let intervalid = setInterval(() => {
+        a++;
+        p.innerHTML = a + "%";
+        innerDiv.style.width = "a";
+
+    }, c * 10);
+    setTimeout(() => {
+        clearInterval(intervalid);
+        btn.innerHTML = "downloaded";
+        console.log("downloaded in " + b + "secs...")
+
+    }, c * 1000);
+
 });
-
 
